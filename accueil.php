9eaafd615cb486a3fc0918f54a0fa4a3c6fdf537
Emma Leger <e.leger@codeur.online>
2020-07-16 16:54:31 +0200

    section 3 : another form contact, linkedin & github

--- accueil.php
+++ accueil.php
@@ -93,7 +93,7 @@
 
 <!-- Button -->
   <div id="show">
-    <a href="" ><button type="button" class="btn btn-outline-light">Show more</button></a>
+    <a href="projets.php"><button type="button" class="btn btn-outline-light">Show more</button></a>
   </div>
 
 <!-- Arrow down -->
@@ -103,110 +103,37 @@
 </section>
 
 <!-- section 3 -->
-
 <section id="section3" class="image-holder">
 
-  <!--Section: Contact v.2-->
-  <section class="mb-4">
+  <!-- Contactez-moi -->
+  <div id="main">
+    Contactez-moi !
+  </div>
 
-      <!--Section heading-->
-      <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-      <!--Section description-->
-      <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-          a matter of hours to help you.</p>
+<!-- formulaire de contact -->
+<form class="contact" action="index.html" method="post">
+  <div class="libelle">
+    <label for="name">Nom :</label>
+    <input type="text" id="name" name="user_name">
+</div>
+<div class="libelle">
+    <label for="mail">E-mail :</label>
+    <input type="email" id="mail" name="user_mail">
+</div>
+<div class="libelle">
+    <label for="msg">Message :</label>
+    <textarea id="msg" name="user_message"></textarea>
+</div>
+<div id="button">
+    <button class="btn btn-outline-light" type="submit">Envoyer le message</button>
+</div>
+</form>
 
-      <div class="row">
-
-          <!--Grid column-->
-          <div class="col-md-9 mb-md-0 mb-5">
-              <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
-                  <!--Grid row-->
-                  <div class="row">
-
-                      <!--Grid column-->
-                      <div class="col-md-6">
-                          <div class="md-form mb-0">
-                              <input type="text" id="name" name="name" class="form-control">
-                              <label for="name" class="">Your name</label>
-                          </div>
-                      </div>
-                      <!--Grid column-->
-
-                      <!--Grid column-->
-                      <div class="col-md-6">
-                          <div class="md-form mb-0">
-                              <input type="text" id="email" name="email" class="form-control">
-                              <label for="email" class="">Your email</label>
-                          </div>
-                      </div>
-                      <!--Grid column-->
-
-                  </div>
-                  <!--Grid row-->
-
-                  <!--Grid row-->
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div class="md-form mb-0">
-                              <input type="text" id="subject" name="subject" class="form-control">
-                              <label for="subject" class="">Subject</label>
-                          </div>
-                      </div>
-                  </div>
-                  <!--Grid row-->
-
-                  <!--Grid row-->
-                  <div class="row">
-
-                      <!--Grid column-->
-                      <div class="col-md-12">
-
-                          <div class="md-form">
-                              <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                              <label for="message">Your message</label>
-                          </div>
-
-                      </div>
-                  </div>
-                  <!--Grid row-->
-
-              </form>
-
-              <div class="text-center text-md-left">
-                  <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
-              </div>
-              <div class="status"></div>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-md-3 text-center">
-              <ul class="list-unstyled mb-0">
-                  <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                      <p>San Francisco, CA 94126, USA</p>
-                  </li>
-
-                  <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                      <p>+ 01 234 567 89</p>
-                  </li>
-
-                  <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                      <p>contact@mdbootstrap.com</p>
-                  </li>
-              </ul>
-          </div>
-          <!--Grid column-->
-
-      </div>
-
-  </section>
-  <!--Section: Contact v.2-->
-
-
-
-
-
+<!-- Réseaux -->
+<div id="social">
+  <a class="reseaux" target="_blank" href="https://www.linkedin.com/in/emma-leger/">Linkedin</a>
+  <a class="reseaux" target="_blank" href="https://github.com/emmaleger58">Github</a>
+</div>
 
 </section>
 
